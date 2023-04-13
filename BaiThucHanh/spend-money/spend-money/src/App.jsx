66bf294aa240a992e7f153ab0c05e30c95ec4f52
duong2601 <duongtit260199@gmail.com>
2023-04-13 17:21:28 +0700

    JS230213-NgocAnh

---
+++ BaiThucHanh/spend-money/spend-money/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components.jsx/NavBar';
+import Products from "./components.jsx/Products"
+function App() {
+  return(
+    <div>
+      <NavBar></NavBar>
+      <Products></Products>
+    </div>
+  )
+}
+
+export default App;
---
+++ BaiThucHanh/spend-money/spend-money/src/components.jsx/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function NavBar() {
+    return(
+        <div>
+            <h3>To spend ....You have a lot of money</h3>
+        </div>
+    )
+}
